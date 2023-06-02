07056f299eda820d51c4c4c2c8385ff08fac2ce1
Sam Teel <sjkteel@hotmail.com>
2023-06-02 10:44:23 -0400

    created hours schedule sections

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
 $(function () {
-
+  var saveButton1 = $('saveBtn1');//first hour save button
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
@@ -15,12 +15,16 @@ $(function () {
   // attribute of each time-block be used to conditionally add or remove the
   // past, present, and future classes? How can Day.js be used to get the
   // current hour in 24-hour time?
+  for (var i = 0; i > 9; i++) {
+    var hourCheck = $();
+  }
+
   //
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
   //
-  // TODO: Add code to display the current date in the header of the page. 
+  // TODO: Add code to display the current date in the header of the page. **DONE**
 
   function dateAndTime() { //gets our date and time
     var today = dayjs().format('MMM DD, YYYY [at] hh:mm:ss a');
